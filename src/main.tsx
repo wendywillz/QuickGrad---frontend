5ca3be6af0d3adc7ace611b9ac4acf10e992d25f
DevVijex <ilechukwuvivian0@gmail.com>
2024-01-15 22:12:52 +0100

    my first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import ResetOtp from "./components/reset_otp.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+// import ResetOtp from "./components/reset_otp.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ResetOtp />
       <App />
     </BrowserRouter>
   </React.StrictMode>
