586c7eaca3e396c2d9e6487d878f5d76348221af
abdrasaq14 <abdrasaq14@gmail.com>
2024-01-16 15:51:51 +0100

    updated the merging and routing to show various routes/path worked on

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { LoginPage } from "./pages/Login";
 import "./App.css";
-import LoginImage from "./assets/loginBackground.png";
+import student_login_page_bg from "./assets/loginBackground.png";
+import lecturer_login_page_bg from "./assets/lecturer_signin_bg.png"
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
       <Routes>
-        
+        <Route path="/" element={<LandingPage />} />
         <Route
           path="/signin"
           element={
@@ -15,18 +17,18 @@ function App() {
               form_title="Sign into QuickGrade"
               id_or_email="Student Registration Number"
               placeholder="Enter your Registration number"
-              backgroundImage={LoginImage}
+              backgroundImage={student_login_page_bg}
             />
           }
         />
         <Route
-          path="/lecturer_signup"
+          path="/lecturer_signin"
           element={
             <LoginPage
               form_title="Sign into QuickGrade"
-              id_or_email="Lecturer ID"
-              placeholder="Enter your staff ID"
-              backgroundImage={LoginImage}
+              id_or_email="Employee ID"
+              placeholder="Enter email"
+              backgroundImage={lecturer_login_page_bg}
             />
           }
         />
