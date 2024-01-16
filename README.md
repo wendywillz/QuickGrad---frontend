ee0587335e9d8b7e59bb42e002700d5c0877b5b8
abdrasaq14 <abdrasaq14@gmail.com>
2024-01-16 14:18:52 +0100

    updated the setup

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ note: there's no dis/lib folder you don't have to worry, your work is compiled o
 There's routing in the project, the signing page can be viewed at `localhost:port/signin`
 kindly include other routes as you work on them.
 
-Cheers!!
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { LoginPage } from "./pages/Login";
 import "./App.css";
 import LoginImage from "./assets/loginBackground.png";
 import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Routes>
+        
         <Route
           path="/signin"
           element={
@@ -13,6 +15,17 @@ function App() {
               form_title="Sign into QuickGrade"
               id_or_email="Student Registration Number"
               placeholder="Enter your Registration number"
+              backgroundImage={LoginImage}
+            />
+          }
+        />
+        <Route
+          path="/lecturer_signup"
+          element={
+            <LoginPage
+              form_title="Sign into QuickGrade"
+              id_or_email="Lecturer ID"
+              placeholder="Enter your staff ID"
               backgroundImage={LoginImage}
             />
           }
