8ccd48bb034e7ee4af60c1e30d73cfc14ada2e3d
abdrasaq14 <119352385+abdrasaq14@users.noreply.github.com>
2024-01-16 22:48:09 +0100

    Update Reset_enter_new_pw.tsx
    Made some few touches to match the figma  design

--- src/pages/Reset_enter_new_pw.tsx
+++ src/pages/Reset_enter_new_pw.tsx
@@ -1,38 +1,45 @@
-import '../styles/Reset_enter_new_pw.css';
-
-
-const ResetEnterNewPasswordPage= () => {
+import "../styles/Reset_enter_new_pw.css";
+import quickgradelogo from "../assets/quick_grade_logo_with_text.png";
+const ResetEnterNewPasswordPage = () => {
   return (
+    <div className="re-enter-password-wrapper">
+      <div className="header">
+        <img src={quickgradelogo} alt="logo png" />
+      </div>
+      <div className="container">
+        <div className="form-container">
+          <h1 className="form-title">Reset Password</h1>
 
-<>
-    <div className="header">
-                 <img src ='/quick.png' alt="logo png"/>
-                 
+          <form className="form">
+            <div className="form-inner-container">
+              <label className="re-enter-password-label" htmlFor="password">New Password</label>
+              <input
+                type="password"
+                placeholder="Enter your new Password"
+                name="Enter your new password"
+                id="password"
+                required
+                className="re-enter-password-input"
+              />
+
+              <br />
+              <label className="re-enter-password-label" htmlFor="confirm-password">Confirm Password</label>
+              <input
+                type="password"
+                placeholder="Confirm New Password"
+                name="confirm-password"
+                id="confirm-password"
+                required
+                className="re-enter-password-input"
+              />
+
+              <button className="btn">Reset Password</button>
             </div>
-    <div className="container">
-          
-        <div className = "form-container">
-                <h1 className="form-title">Reset Password</h1>
-
-                 <form className = "form">
-                      <div className="form-inner-container">
-                        
-                            <label htmlFor="password">New Password</label>
-                            <input type="password" placeholder="Enter your new Password" name="Enter your new password" id="password" required/>
- 
-                                <br/>
-                             <label htmlFor="confirm-password">Confirm Password</label>
-                             <input type="password" placeholder="Confirm New Password" name="confirm-password" id="confirm-password" required/>
- 
-                             <button className="btn">Reset Password</button>
-                             
-                      </div>
-                </form>
+          </form>
         </div>
+      </div>
     </div>
-</>
   );
-}
- 
-export default  ResetEnterNewPasswordPage;
- 
+};
+
+export default ResetEnterNewPasswordPage;
