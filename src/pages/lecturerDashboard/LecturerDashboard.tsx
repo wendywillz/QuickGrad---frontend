3fb2bb9dfbd2c9cac542aad813ace2977b2dfd7d
xeth99 <you@examemmanuelfemi01@gmanil.com>
2024-01-28 14:40:54 +0100

    linked the backend of lecturer dashboard with the frontend and redid the styling

--- src/pages/lecturerDashboard/LecturerDashboard.tsx
+++ src/pages/lecturerDashboard/LecturerDashboard.tsx
@@ -2,40 +2,69 @@ import "../studentDashboard/Dashboard.css";
 import "../../assets/menu-board.png";
 import SideBar from "../../components/sidebar/sideBar";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Lecturer {
+  lecturerId: string;
+  lastName: string;
+  department: string;
+  faculty: string;
+}
+
+interface Exam {
+  courseCode: string;
+  department: string;
+  examDate: Date;
+  examDuration: string;
+  venue: string;
+  registered: string;
+}
 
 function LecturerDashboard() {
-  // const navigate = useNavigate();
-  // const fetchDashboardDisplay = () => {
-  //   axios
-  //     .get(`http://localhost:3000/lecturers/dashboard`, {
-  //       withCredentials: true,
-  //     })
-  //     .then((response) => {
-  //       const result = response;
-  //       if (result.status === 200) {
-  //         if (result.data.lectuerUnauthorizedError) {
-  //           navigate("/lecturers/signin");
-  //         } else if (result.data.lecturerAuthorized) {
-  //           navigate("/lecturers/dashboard");
-  //         }
-  //       } else {
-  //         navigate("/lecturers/signin");
-  //       }
-  //     })
-  //     .catch((error) => console.log(error));
-  // };
-  // fetchDashboardDisplay();
+  const [lecturerData, setLecturerData] = useState<Lecturer | null>(null);
+  const [examData, setExamData] = useState<Exam[]>([]);
+  const [selectedSemester, setSelectedSemester] = useState<string>("First");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/lecturers/dashboard`,
+          {
+            withCredentials: true,
+            params: { semester: selectedSemester },
+          }
+        );
+  
+        if (res.status === 200 && res.data.lecturer.lecturerId) {
+          setLecturerData(res.data.lecturer);
+          setExamData(res.data.exam);
+          console.log(res.data);
+        }
+      } catch (error) {
+        console.log("Error fetching dashboard data:", error);
+      }
+    };
+  
+    fetchData();
+  }, [selectedSemester]);
+  
+
   return (
     <div className="dashboard">
-      <section className="hero">
+      {lecturerData && (
+        <section className="hero">
         <div className="hero-content">
           <img
             className="img"
             src="https://c.animaapp.com/IX1zE9E9/img/notification.svg"
           />
-          <div className="text-wrapper">Welcome, 22/23/07/019</div>
+          <div className="text-wrapper">Welcome, {lecturerData.lecturerId}</div>
         </div>
       </section>
+      )}
+      
       {/* Sidebar */}
       <div className="text-wrapper-2">Dashboard</div>
       <SideBar>
@@ -90,90 +119,67 @@ function LecturerDashboard() {
       </SideBar>
       {/* Main content */}
       <main className="frame-5">
-        <div className="frame-wrapper">
+        {lecturerData && (
+          <div className="frame-wrapper">
           <p className="lecturer">
-            Lecturer (Dr.), Department of Chemistry.
+            Lecturer Dr. {lecturerData.lastName}, Department of {lecturerData.department}.
             <br />
-            Faculty of Science.
+            {lecturerData.faculty}.
             <br />
             Camouflage University. <br />
             Atlanta, Nigeria.
           </p>
         </div>
+        )}
+
+        {examData && examData.length > 0 && (
         <div className="div-wrapper">
           <div className="frame-7">
             <div className="frame-8">
               <div className="frame-9">
                 <div className="wrapper-text">
-                  <div className="text">2022/2023 : Second Semester</div>
+                  <select value={selectedSemester} onChange={(e) => setSelectedSemester(e.target.value)}>
+                  <option className="text" value="First">2023/2024 : {selectedSemester}</option>
+                  </select>
                   <div className="course">Course Examination TimeTable</div>
                 </div>
               </div>
             </div>
             <div className="frame-10">
-              <div className="frame-12-lecturer">
-                <div className="text-wrapper-8">Course Code</div>
-                <div className="text-wrapper-8">Department</div>
-                <div className="text-wrapper-8">Date/Time</div>
-                <div className="text-wrapper-8">Venue</div>
-                <div className="text-wrapper-8">Registered</div>
-                <div className="text-wrapper-8">Action</div>
-              </div>
-              <div className="frame-13">
-                <div className="frame-14-lecturer">
-                  <div className="text-wrapper-10-lecturer">CE 522</div>
-                  <div className="text-wrapper-10-lecturer">
-                    Chemical Engineering
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    10 Aug, 2013 / <br /> 2:10pm
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    Campus E-center
-                  </div>
-                  <div className="text-wrapper-10-lecturer">211</div>
-                  <div>
-                    <button type="submit" className="text-wrapper-12">
-                      Set Action
-                    </button>
-                  </div>
+                <div className="frame-12-lecturer">
+                  <div className="lecturerTexts">Course Code</div>
+                  <div className="lecturerTexts">Department</div>
+                  <div className="lecturerTexts">Date/Time</div>
+                  <div className="lecturerTexts">Venue</div>
+                  <div className="lecturerTexts">Registered</div>
+                  <div className="lecturerTexts">Action</div>
                 </div>
-                <div className="frame-14-lecturer">
-                  <div className="text-wrapper-10-lecturer">CE 522</div>
-                  <div className="text-wrapper-10-lecturer">
-                    Chemical Engineering
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    10 Aug, 2013 / <br /> 2:10pm
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    Campus E-center
-                  </div>
-                  <div className="text-wrapper-10-lecturer">211</div>
-                  <div>
-                    <button className="text-wrapper-12">Set Action</button>
-                  </div>
+                <div className="frame-13-lecturer">
+                <table>
+                  <tbody>
+                    {examData.map((exam, index) => (
+                    <tr key={index}>
+                      <td className="text-wrapper-10-lecturer">{exam.courseCode}</td>
+                      <td className="text-wrapper-10-lecturer">{exam.department}</td>
+                      <td className="text-wrapper-10-lecturer">
+                        {exam.examDate.toLocaleString()} / <br /> {exam.examDuration}
+                      </td>
+                      <td className="text-wrapper-10-lecturer">{exam.venue}</td>
+                      <td className="text-wrapper-10-lecturer">{exam.registered}</td>
+                      <td>
+                        <button type="submit" className="submit-button-for-lecturer">
+                          Set Action
+                        </button>
+                      </td>
+                    </tr>
+                    ))}
+                  </tbody>
+                </table>
                 </div>
-                <div className="frame-14-lecturer">
-                  <div className="text-wrapper-10-lecturer">CE 522</div>
-                  <div className="text-wrapper-10-lecturer">
-                    Chemical Engineering
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    10 Aug, 2013 / <br /> 2:10pm
-                  </div>
-                  <div className="text-wrapper-10-lecturer">
-                    Campus E-center
-                  </div>
-                  <div className="text-wrapper-10-lecturer">211</div>
-                  <div>
-                    <button className="text-wrapper-12">Set Action</button>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
+        )}
       </main>
     </div>
   );
